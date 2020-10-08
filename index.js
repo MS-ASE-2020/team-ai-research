4c3edcfb4a9fb71e9e3913057b7fbe523bed49c0
iBug <git@ibugone.com>
2020-10-09 00:49:56 +0800

    Update package.json

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       nodeIntegration: true
     }
