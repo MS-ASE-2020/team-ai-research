3dc4ea1310ec10700c541a7fbe249185970af1a4
taoky <taoky99@outlook.com>
2020-10-09 13:27:46 +0800

    demo0 upload

---
+++ webpack.web.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './web/index.js',
+  mode: 'development',
+
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'web', '__build__'),
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  }
+};
