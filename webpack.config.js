b7b90b59fffb45a0b0394e3a5e3b00fd5ed23bcf
iBug <git@ibugone.com>
2020-11-06 02:09:21 +0800

    Merge commit 091e422 trying to solve db import issue

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  externals: {
+    "aws-sdk": "aws-sdk",
+    pg: "pg",
+    "pg-query-stream": "pg-query-stream",
+    mssql: "mssql",
+    "mssql/lib/base": "mssql/lib/base",
+    "mssql/package.json": "mssql/package.json",
+    mysql: "mysql",
+    mysql2: "mysql2",
+    oracle: "oracle",
+    oracledb: "oracledb",
+    tedious: "tedious",
+  },
+};
