8f365fab8316e5e71b5bdd0f2daf28612a0bcd7a
Joe Linsar <45262037+TheLitFire@users.noreply.github.com>
2020-11-11 20:58:57 +0800

    functions for #3

--- src/db.js
+++ src/db.js
@@ -3,57 +3,65 @@ const DB_FILE = "papera.db";
 const process = require('process');
 
 function connectDatabase() {
-  var db = new Database(DB_FILE, { verbose: console.log });
+  try {
+    var db = new Database(DB_FILE, {verbose: console.log, fileMustExist: true});
+  } catch (error) {
+    db = new Database(DB_FILE, {verbose: console.log});
+    /*
+    * `paper` entity table
+    * - `ID`: using the "rowid" technic of SQlite3
+    * - `keywords`: a list of keywords seperated by some special charactor
+    * - `lastedit`: use TEXT datatype for date-time info, see https://www.sqlitetutorial.net/sqlite-date/
+    * - `QandA`: Question&Answer notes of this paper
+    * - `annotations`
+    */
+    db.prepare(`CREATE TABLE paper(
+                  ID INTEGER PRIMARY KEY,
+                  name TEXT UNIQUE NOT NULL,
+                  title TEXT NOT NULL,
+                  keywords TEXT,
+                  year INTEGER,
+                  conference TEXT,
+                  lastedit TEXT,
+                  QandA,
+                  annotations
+                );`).run();
+    /*
+    * `folder` entity table
+    * - `ID`: using the "rowid" technic of SQlite3
+    * - `createtime`: use TEXT datatype for date-time info, see https://www.sqlitetutorial.net/sqlite-date/
+    */
+    db.prepare(`CREATE TABLE folder(
+                  ID INTEGER PRIMARY KEY,
+                  name TEXT NOT NULL,
+                  description TEXT,
+                  createtime TEXT,
+                  fatherID INTEGER,
+                  FOREIGN KEY (fatherID) REFERENCES folder (ID)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                  UNIQUE(name, fatherID)
+                );`).run();
+    /*
+    * `paperInFolder` relation table
+    */
+    db.prepare(`CREATE TABLE paperInFolder(
+                  paperID INTEGER,
+                  folderID INTEGER,
+                  FOREIGN KEY (paperID) REFERENCES paper (ID)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                  FOREIGN KEY (folderID) REFERENCES folder (ID)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                  PRIMARY KEY (paperID, folderID)
+                );`).run();
+    /*
+    * insert an explict root directory, so that the UNIQUE constraint in table `folder` works well for subdirectorys of '/'
+    */
+    db.prepare(`INSERT INTO folder VALUES (1, '/', 'root', datetime('now','localtime'), null);`).run();
+
+    db.prepare(`CREATE UNIQUE INDEX idx_paper_name ON paper (name);`).run();
+  }
+
   process.on('exit', () => db.close());
-
-  /*
-   *  `paper` entity table
-   *  - `paperID`: using the "rowid" technic of SQlite3
-   *  - `paperLastedit`: use TEXT datatype for date-time info, see https://www.sqlitetutorial.net/sqlite-date/
-   *  - `paperQA`: Question&Answer notes of this paper. Notice that this is a sub-table property.
-   *  - `paperMarks`
-   */
-  db.prepare(`CREATE TABLE IF NOT EXISTS paper(
-                paperID INTEGER PRIMARY KEY,
-                paperName TEXT UNIQUE NOT NULL,
-                paperTitle TEXT NOT NULL,
-                paperKeywords TEXT,
-                paperYear INTEGER,
-                paperConference TEXT,
-                paperLastedit TEXT,
-                paperQA,
-                paperMarks
-              );`).run();
-  /*
-   *  `folder` entity table
-   *  - `folderID`: using the "rowid" technic of SQlite3
-   *  - `folderCreatetime`: use TEXT datatype for date-time info, see https://www.sqlitetutorial.net/sqlite-date/
-   */
-  db.prepare(`CREATE TABLE IF NOT EXISTS folder(
-                folderID INTEGER PRIMARY KEY,
-                folderName TEXT NOT NULL,
-                folderDescription TEXT,
-                folderCreatetime TEXT,
-                folderFatherID INTEGER,
-                FOREIGN KEY (folderFatherID) REFERENCES folder (folderID)
-                  ON DELETE CASCADE ON UPDATE CASCADE,
-                UNIQUE(folderName, folderFatherID)
-              );`).run();
-  /*
-   *  `paperInFolder` relation table
-   */
-  db.prepare(`CREATE TABLE IF NOT EXISTS paperInFolder(
-                PIFpaperID INTEGER,
-                PIFfolderID INTEGER,
-                FOREIGN KEY (PIFpaperID) REFERENCES paper (paperID)
-                  ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (PIFfolderID) REFERENCES folder (folderID)
-                  ON DELETE CASCADE ON UPDATE CASCADE,
-                PRIMARY KEY (PIFpaperID, PIFfolderID)
-              );`).run();
-
-  db.prepare(`INSERT OR IGNORE INTO folder VALUES (0, '/', 'root', '', null);`).run();
-
   return db;
 }
 
@@ -61,21 +69,88 @@ function closeDatabase(db) {
   db.close();
 }
 
-function example(db) {
-  const insert = db.prepare('INSERT INTO paper (paperName, paperTitle) VALUES (@name, @title)');
+/**
+ * Save annotations, Q&As and/or other properties into table `paper`,
+ * no matter whether or not it is new for papera.
+ * Set `properties.ID` to `null`/`undefined`/`false` for paper creation.
+ * @param { BetterSqlite3.Database } db 
+ * @param {{ID: Number, name: String, title: String, keywords: String, year: Number, conference: String, lastedit: String, QandA, annotations}} properties 
+ * @returns ID of created/updated paper.
+ * @throws error object thrown by SQlite.
+ */
+function savePaper(db, properties) {
+  let sqlStmt;
+  if (properties.ID) {
+    sqlStmt = db.prepare(`UPDATE paper
+                          SET name = @name,
+                          title = @title,
+                          keywords = @keywords,
+                          year = @year,
+                          conference = @conference,
+                          lastedit = @lastedit,
+                          QandA = @QandA,
+                          annotations = @annotations;`);
+  } else {
+    sqlStmt = db.prepare(`INSERT INTO paper (name, title, keywords, year, conference, lastedit, QandA, annotations)
+                      VALUES (@name, @title, @keywords, @year, @conference, @lastedit, @QandA, @annotations);`);
+  }
+  try {
+    db.transaction(() => {
+      sqlStmt.run(properties);
+    })();
+    if (!properties.ID) {
+      properties.ID = db.prepare(`SELECT ID FROM paper WHERE name = ?;`).get(properties.name).ID;
+    }
+  } catch (error) {
+    console.error(error)
+    throw error;
+  }
+  return properties.ID;
+}
 
-  const insertMany = db.transaction((papers) => {
-    for (const paper of papers) insert.run(paper);
-  });
+/**
+ * List papers in a specific folder with given folder ID.
+ * List all papers in papera when `folderID` is `null` or `false` or `undefined`
+ * @param { BetterSqlite3.Database } db 
+ * @param { Number } folderID 
+ * @returns { Array< { ID:Number, name:String } > }
+ * @throws error object thrown by SQlite.
+ */
+function listPaper(db, folderID) {
+  let sqlStmt;
+  if (folderID) {
+    sqlStmt = db.prepare(`SELECT ID, name FROM paper
+                          WHERE ID IN (
+                            SELECT paperID from paperInFolder
+                            WHERE folderID = ?
+                          );`).bind(folderID);
+  } else {
+    sqlStmt = db.prepare(`SELECT ID, name FROM paper;`);
+  }
+  try {
+    var result = sqlStmt.run(properties).all();
+  } catch (error) {
+    console.error(error)
+    throw error;
+  }
+  return result;
+}
 
-  insertMany([
-    { name: 'test1', title: '2' },
-    { name: 'test2', title: '3' },
-    { name: 'test3', title: '4' },
-  ]);
+function getAnnotation(db, paperID) {
+  let sqlStmt = db.prepare(`SELECT annotations FROM paper WHERE ID = ?`);
+  try {
+    var result = sqlStmt.get(paperID).annotations
+  } catch (error) {
+    console.error(error)
+    throw error;
+  }
+  return result;
 }
 
 module.exports = {
   connect: connectDatabase,
-  close: closeDatabase
+  close: closeDatabase,
+  savePaper: savePaper,
+  listPaper: listPaper,
+  getAnnotation: getAnnotation
 };
