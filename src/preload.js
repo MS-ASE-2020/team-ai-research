90efe9b5cb91a989ae40f691c0f62f578bd6a79b
taoky <taoky99@outlook.com>
2020-11-30 11:34:23 +0800

    overwrite broken alert and confirm in electron

--- src/preload.js
+++ src/preload.js
@@ -3,7 +3,7 @@
 //   ipcRenderer
 // } = require("electron");
 
-const { app, remote } = require("electron").remote;
+const { app, dialog } = require("electron").remote;
 
 const db = require("./db");
 const filesystem = require("./filesys");
@@ -16,5 +16,31 @@ window.api = {
   version_info: process.versions,
   database: db,
   filesystem: filesystem,
-  userDataDir: app.getPath('userData') || remote.app.getPath('userData'),
+  userDataDir: app.getPath('userData'),
+};
+
+// overwrite alert and confirm, as they stuck in electron
+window.alert = (detail) => {
+  const options = {
+    type: 'warning',
+    buttons: ['OK'],
+    defaultId: 0,
+    cancelId: 0,
+    detail: detail,
+    message: ''
+  };
+  dialog.showMessageBoxSync(null, options);
+};
+
+window.confirm = (detail) => {
+  const options = {
+    type: 'question',
+    buttons: ['OK', 'Cancel'],
+    defaultId: 0,
+    cancelId: 1,
+    detail: detail,
+    message: ''
+  };
+  const buttonIdx = dialog.showMessageBoxSync(null, options);
+  return buttonIdx === 0;
 };
