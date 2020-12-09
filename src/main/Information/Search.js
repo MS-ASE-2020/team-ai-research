c169e632b8e2dc9dee61b75787ef1d4784cb4904
Double680 <woxin_feiyang0503@126.com>
2020-12-09 16:34:11 +0800

    checkbox

--- src/main/Information/Search.js
+++ src/main/Information/Search.js
@@ -5,14 +5,18 @@ export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortKey: "name"
+      title: false,
+      content: false,
+      keywords: false,
+      year: false,
+      conference: false 
     };
   }
 
   handleChange(event) {
-    const value = event.target.value;
+    const name = event.target.name;
     this.setState({
-      sortKey: value
+      [[name]]: !this.state[[name]]
     });
   }
 
@@ -21,12 +25,14 @@ export default class Search extends Component {
       <div>
         <input type="text" /> &nbsp;
         Search By:
-        <input type="checkbox" value="title" />title
-        <input type="checkbox" value="content" />content
-        <input type="checkbox" value="keywords" />keywords
-        
+        <input type="checkbox" name="title" onChange={this.handleChange.bind(this)}/>Name
+        <input type="checkbox" name="content" onChange={this.handleChange.bind(this)}/>Title
+        <input type="checkbox" name="keywords" onChange={this.handleChange.bind(this)}/>Keywords
+        <input type="checkbox" name="year" onChange={this.handleChange.bind(this)}/>Year
+        <input type="checkbox" name="conference" onChange={this.handleChange.bind(this)}/>Conference
+        &nbsp;
         <button>Order</button> &nbsp;
-        <button onClick={() => alert(this.state.sortKey)}>Search</button>
+        <button onClick={() => alert(JSON.stringify(this.state))}>Search</button>
       </div>
     );
   }
