6a3cae775d824f63d0e54904082ad81fa1b06bbe
Double680 <woxin_feiyang0503@126.com>
2020-12-11 14:30:10 +0800

    Fix bug of search

--- src/db.js
+++ src/db.js
@@ -481,22 +481,23 @@ function saveFolderOfPaper(db, paperID, folderIDs) {
  */
 function searchPaperInFolder(db, folderID, searchBy, queryText, recursive=false) {
   let paperIDs = listPaper(db, folderID, recursive);
-  let paperIDsStr = "(" + paperIDs.map(x => String(x)).join(",") + ")";
+  let paperIDsStr = "(" + paperIDs.map(x => String(x.ID)).join(",") + ")";
   let searchByStrs = ["pName", "pTitle", "pKeywords", "pYear", "pConference", "pLastedit", "pQandA", "pAnnotations", "pContent", "fPath", "fDescription", "fCreatetime"];
   let searchByStr = ' ';
   for (const colName of searchByStrs) {
     if (searchBy[colName]) {
-      searchByStr += colName + ' ';
+      searchByStr = searchByStr + colName + ' ';
     }
   }
-  let result = db.prepare(`
+  let sql = `
     SELECT pID AS ID, pName AS name, snippet(paperAndFolderForSearch, -1, '<b>', '</b>', '......', 64) AS matcher
     FROM paperAndFolderForSearch
     WHERE
       (pID IN ` + paperIDsStr + `) AND
-      (paperAndFolderForSearch MATCH '{${searchByStr}}: ${queryText}')
+      (paperAndFolderForSearch MATCH '{ ` + searchByStr + `}: ` + queryText + `')
     ORDER BY rank;
-  `).all();
+  `;
+  let result = db.prepare(sql).all();
   return result;
 }
 
--- src/main/Information/Search.js
+++ src/main/Information/Search.js
@@ -4,12 +4,22 @@ export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: "",
       name: false,
       title: false,
       keywords: false,
       year: false,
-      conference: false 
+      conference: false,
+      recursive: false,
+      searchItem: []
     };
+  }
+
+  handleTextChange(event) {
+    const value = event.target.value;
+    this.setState({
+      text: value
+    });
   }
 
   handleChange(event) {
@@ -19,12 +29,27 @@ export default class Search extends Component {
     });
   }
 
+  handleSearch() {
+    let searchItem = window.api.database.searchPaperInFolder(window.db, this.props.folderID, {
+      pName: this.state.name,
+      pTitle: this.state.title,
+      pKeywords: this.state.keywords,
+      pYear: this.state.year,
+      pConference: this.state.conference
+    }, this.state.text, this.state.recursive);
+    alert(JSON.stringify(searchItem));
+  }
+
   render() {
     return (
       <div>
-        <input type="text" style={{width: 500}}/> &nbsp;
+        <input 
+          type="text" 
+          style={{width: 500}}
+          onChange={this.handleTextChange.bind(this)}
+        /> &nbsp;
         <button onClick={() => alert("Placeholder!")}>Order</button> &nbsp;
-        <button onClick={() => alert(JSON.stringify(this.state))}>Search</button>
+        <button onClick={this.handleSearch.bind(this)}>Search</button>
         <div>
           Search By: &nbsp;
           <input type="checkbox" name="name" onChange={this.handleChange.bind(this)}/>Name &nbsp;
@@ -32,6 +57,8 @@ export default class Search extends Component {
           <input type="checkbox" name="keywords" onChange={this.handleChange.bind(this)}/>Keywords &nbsp;
           <input type="checkbox" name="year" onChange={this.handleChange.bind(this)}/>Year &nbsp;
           <input type="checkbox" name="conference" onChange={this.handleChange.bind(this)}/>Conference &nbsp;
+          Recursive: &nbsp;
+          <input type="checkbox" name="recursive" onChange={this.handleChange.bind(this)}/> &nbsp;
         </div>
         &nbsp; 
       </div>
