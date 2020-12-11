030ea3e3ba9d9bb27cab256582a31c40384ab647
Double680 <woxin_feiyang0503@126.com>
2020-12-11 22:11:29 +0800

    search

--- src/main/Information/Search.js
+++ src/main/Information/Search.js
@@ -1,19 +1,206 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from "react";
 
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Radio from '@material-ui/core/Radio';
 import Checkbox from '@material-ui/core/Checkbox';
-import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
 import TextField from '@material-ui/core/TextField';
-import { Check } from "@material-ui/icons";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function SearchBy(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <span>
+      <Button 
+        aria-controls="simple-menu" 
+        aria-haspopup="true" 
+        onClick={handleClick}
+        endIcon={<ArrowDropDownIcon />}
+        variant="outlined"
+      >
+        By
+      </Button>
+      <Menu
+        id="simple-menu"
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="pName" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pName}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Name"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pTitle" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pTitle}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Title"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pKeywords" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pKeywords}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Keywords"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pYear" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pYear}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Year"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pConference" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pConference}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Conference"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pQandA" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pQandA}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Q and A"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pAnnotations" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pAnnotations}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Annotations"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="pContent" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.pContent}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Content"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="fPath" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.fPath}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Folder Path"
+          />
+        </MenuItem>
+        <MenuItem>
+          <FormControlLabel
+            control={
+              <Checkbox 
+                name="fDescription" 
+                onChange={props.handleSearchBy} 
+                size="small"
+                defaultChecked={props.searchBy.fDescription}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Folder Description"
+          />
+        </MenuItem>
+      </Menu>
+    </span>
+  );
+} 
 
 export default class Search extends Component {
   constructor(props) {
@@ -75,13 +262,27 @@ export default class Search extends Component {
         <Accordion 
           square
           key={k}
-          style={{width: "800px"}}>
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography>{this.state.searchItem[k].name}</Typography>
+          style={{width: "880px"}}>
+          <AccordionSummary 
+            aria-controls="panel1d-content" 
+            id="panel1d-header"
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography>
+              <IconButton 
+                size="small"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  alert(JSON.stringify(this.state.searchItem[k].ID));
+                }}>
+                <SendIcon fontSize="inherit"/>
+              </IconButton> &nbsp;
+              {this.state.searchItem[k].name}
+            </Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography color="textSecondary">
-              {this.state.searchItem[k].matcher}
+              <div dangerouslySetInnerHTML={{ __html: this.state.searchItem[k].matcher }} />
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -102,10 +303,13 @@ export default class Search extends Component {
             variant="outlined"
             size="small"
             style={{
-              width: "550px"
+              width: "600px"
             }}
             onChange={this.handleTextChange.bind(this)}
-          />
+          /> &nbsp;
+          <SearchBy 
+            searchBy={this.state.searchBy}
+            handleSearchBy={this.handleSearchBy.bind(this)}/>
           <FormControlLabel
             control={
               <Checkbox
@@ -129,135 +333,6 @@ export default class Search extends Component {
         </div>
         <br/>
         <Divider />
-        <div>
-          <FormLabel component="legend">Search By Paper Information: </FormLabel>
-          <div>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  name="pName" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Name"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pTitle" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Title"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pKeywords" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Keywords"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pYear" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Year"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pConference" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Conference"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pQandA" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Q&A"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pAnnotations" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Annotations"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="pContent" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Content"
-            />       
-          </div>
-          <FormLabel component="legend">Search By Folder Information: </FormLabel>
-          <div>
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="fPath" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Path"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox 
-                  name="fDescription" 
-                  onChange={this.handleSearchBy.bind(this)} 
-                  size="small"
-                  defaultChecked
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
-                />
-              }
-              label="Description"
-            />
-          </div> 
-        </div>
-        <Divider />
         <br/>
         <div>
           {searchResult}
@@ -267,4 +342,3 @@ export default class Search extends Component {
   }
 }
 
-    
