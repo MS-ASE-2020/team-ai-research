a0d305618088dcab528a395103b8af64664343a3
Double680 <woxin_feiyang0503@126.com>
2020-12-11 18:03:24 +0800

    search with material-UI

--- src/main/Information/Search.js
+++ src/main/Information/Search.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Radio from '@material-ui/core/Radio';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
+import { Check } from "@material-ui/icons";
+
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       text: "",
-      name: false,
-      title: false,
-      keywords: false,
-      year: false,
-      conference: false,
+      searchBy: {
+        pName: true,
+        pTitle: true,
+        pKeywords: true,
+        pYear: true,
+        pConference: true,
+        pLastedit: true,
+        pQandA: true,
+        pAnnotations: true,
+        pContent: true,
+        fPath: true,
+        fDescription: true,
+        fCreatetime: true,
+      },
       recursive: false,
       searchItem: []
     };
@@ -22,47 +46,225 @@ export default class Search extends Component {
     });
   }
 
-  handleChange(event) {
+  handleSearchBy(event) {
     const name = event.target.name;
+    let searchBy = JSON.parse(JSON.stringify(this.state.searchBy));
+    searchBy[[name]] = !this.state.searchBy[[name]];
     this.setState({
-      [[name]]: !this.state[[name]]
+      searchBy: searchBy
     });
   }
 
-  handleSearch() {
-    let searchItem = window.api.database.searchPaperInFolder(window.db, this.props.folderID, {
-      pName: this.state.name,
-      pTitle: this.state.title,
-      pKeywords: this.state.keywords,
-      pYear: this.state.year,
-      pConference: this.state.conference
-    }, this.state.text, this.state.recursive);
-    alert(JSON.stringify(searchItem));
+  SubmitSearch() {
+    let searchItem = window.api.database.searchPaperInFolder(
+      window.db,
+      this.props.folderID,
+      this.state.searchBy,
+      this.state.text,
+      this.state.recursive
+    );
+    this.setState({
+      searchItem: searchItem
+    });
   }
 
   render() {
+    let searchResult = [];
+    for (let k = 0; k < this.state.searchItem.length; k++) {
+      searchResult.push(
+        <Accordion 
+          square
+          key={k}
+          style={{width: "800px"}}>
+          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+            <Typography>{this.state.searchItem[k].name}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography color="textSecondary">
+              {this.state.searchItem[k].matcher}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      );
+    }
+    if (this.state.searchItem.length === 0) {
+      searchResult.push(
+        <div>
+          No matched results.
+        </div>
+      );
+    }
     return (
       <div>
-        <input 
-          type="text" 
-          style={{width: 500}}
-          onChange={this.handleTextChange.bind(this)}
-        /> &nbsp;
-        <button onClick={() => alert("Placeholder!")}>Order</button> &nbsp;
-        <button onClick={this.handleSearch.bind(this)}>Search</button>
         <div>
-          Search By: &nbsp;
-          <input type="checkbox" name="name" onChange={this.handleChange.bind(this)}/>Name &nbsp;
-          <input type="checkbox" name="title" onChange={this.handleChange.bind(this)}/>Title &nbsp;
-          <input type="checkbox" name="keywords" onChange={this.handleChange.bind(this)}/>Keywords &nbsp;
-          <input type="checkbox" name="year" onChange={this.handleChange.bind(this)}/>Year &nbsp;
-          <input type="checkbox" name="conference" onChange={this.handleChange.bind(this)}/>Conference &nbsp;
-          Recursive: &nbsp;
-          <input type="checkbox" name="recursive" onChange={this.handleChange.bind(this)}/> &nbsp;
+          <TextField 
+            label="Search" 
+            variant="outlined"
+            size="small"
+            style={{
+              width: "550px"
+            }}
+            onChange={this.handleTextChange.bind(this)}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={() => this.setState({
+                  recursive: !this.state.recursive
+                })}
+                size="small"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            }
+            label="Recursive:"
+            labelPlacement="start"
+          /> &nbsp; &nbsp;
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            startIcon={<SearchIcon />}
+            onClick={() => this.SubmitSearch()}
+          >Search</Button>
         </div>
-        &nbsp; 
+        <br/>
+        <Divider />
+        <div>
+          <FormLabel component="legend">Search By Paper Information: </FormLabel>
+          <div>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="pName" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Name"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pTitle" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Title"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pKeywords" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Keywords"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pYear" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Year"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pConference" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Conference"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pQandA" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Q&A"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pAnnotations" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Annotations"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="pContent" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Content"
+            />       
+          </div>
+          <FormLabel component="legend">Search By Folder Information: </FormLabel>
+          <div>
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="fPath" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Path"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  name="fDescription" 
+                  onChange={this.handleSearchBy.bind(this)} 
+                  size="small"
+                  defaultChecked
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Description"
+            />
+          </div> 
+        </div>
+        <Divider />
+        <br/>
+        <div>
+          {searchResult}
+        </div> 
       </div>
     );
   }
 }
 
+    
